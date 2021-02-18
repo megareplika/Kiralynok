2c05a8083ee0eab017fb75d2d402888a6bbd32b4
megareplika <megareplika@gmail.com>
2021-02-18 10:22:19 +0100

    4. feladat kész

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 
 public class Kiralynok {
     public static void main(String[] args) throws IOException {
-        
+        Tabla tabla = new Tabla('#');
+        //4.feladat
+        System.out.println("4. feladat");
+        tabla.megjelenit();
     }
-    
 }
