cf2a91e9e285d66a8bcd90cb8cb0f6a9c98e21ae
megareplika <megareplika@gmail.com>
2021-02-18 10:40:09 +0100

    oop 6.feladat

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -3,11 +3,30 @@ package kiralynok;
 import java.io.IOException;
 
 public class Kiralynok {
+    
+    private Tabla tabla;
+
+    public Kiralynok() {
+        this.tabla = new Tabla('#');
+    }
+    
+    
     public static void main(String[] args) throws IOException {
-        Tabla tabla = new Tabla('#');
+        Kiralynok kir = new Kiralynok();
+        kir.megoldasok();
+    }
+    
+    public void megoldasok(){
+        feladat4();
+        feladat6();
+    }
+    
+    private void feladat4(){
         //4.feladat
         System.out.println("4. feladat");
         tabla.megjelenit();
+    }
+    private void feladat6(){
         //6.feladat
         System.out.println("6. feladat");
         tabla.elhelyez(8);
